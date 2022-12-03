eb14881f11808f08e0f8a94b7128804437e58c46
vasanth12345 <vasanthvp1611@gmail.com>
2022-12-03 18:44:39 +0530

    2nd commit

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!  deployes"));
+const data = [
+  {
+    name: "test 1",
+    age: 20,
+    des: "hayyy test 1",
+  },
+  {
+    name: "test 2",
+    age: 20,
+    des: "hayyy test 1",
+  },
+  {
+    name: "test 3",
+    age: 20,
+    des: "hayyy test 1",
+  },
+];
+
+app.get("/", (req, res) => res.send("Hello World! /api/data"));
+app.get("/api/data", (req, res) => res.send(data));
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${port}!`)
 );
